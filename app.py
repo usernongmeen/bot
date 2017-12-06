3a1297a58a49184e8cf118fba8f21e7cac79b4e6
usernongmeen <34308392+usernongmeen@users.noreply.github.com>
2017-12-07 00:01:26 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def bot():
         reply(replyToken, ['Only text is allowed.'])
         return 'OK',200
     
-    text = msg_in_json["events"][0]['originalContentUrl'].lower().strip()
+    text = msg_in_json["events"][0]['message']['originalContentUrl']
 
     # ตอบข้อความ "นี่คือรูปแบบข้อความที่รับส่ง" กลับไป
     replyStack.append('นี่คือรูปแบบข้อความที่รับส่ง')
